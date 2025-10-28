9b4d91ae28923592b9c6cc9c34f8b1d410c7f3be
nazo <nazyucel@Nazs-MacBook-Air.local>
2025-10-28 12:44:25 -0700

    Add 'equiv' example, update test, copy nodes/edges to inst/extdata/examples/equiv

--- tests/testthat/test-parser-fixtures.R
+++ tests/testthat/test-parser-fixtures.R
@@ -22,5 +22,5 @@ test_that("parser fixtures are self-consistent", {
                 info = paste("Unknown `to` ids:",   paste(unknown_to,   collapse=", ")))
   }
   
-  for (ex in c("rats","pumps", "dogs","seeds", "surgical_fixed", "surgical_random", "salm")) check_one(ex)
+  for (ex in c("rats","pumps", "dogs","seeds", "surgical_fixed", "surgical_random", "salm","equiv")) check_one(ex)
 })
